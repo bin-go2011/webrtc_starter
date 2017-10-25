3f1effc33bb1758053a35f27b5a779d3bdf8cce5
bingo2013 <bin.go2011@foxmail.com>
2017-10-25 11:50:37 +0800

    can work

--- test/simple_app.cc
+++ test/simple_app.cc
@@ -3,26 +3,25 @@
 #include "api/peerconnectioninterface.h"
 #include "api/test/fakeconstraints.h"
 #include "media/engine/webrtcvideocapturerfactory.h"
-#include <iostream>
+
 int main(int argc, char* argv[]) {
-  std::cout << "Hello World" << std::endl;
-// something from base
-// rtc::Thread* thread = rtc::Thread::Current();
+  // something from base
+  rtc::Thread* thread = rtc::Thread::Current();
 
-// something from p2p
-// std::unique_ptr<rtc::BasicPacketSocketFactory> socket_factory(
-//   new rtc::BasicPacketSocketFactory());
+  // something from p2p
+  std::unique_ptr<rtc::BasicPacketSocketFactory> socket_factory(
+    new rtc::BasicPacketSocketFactory());
 
-// something from api
-rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface>
-  peer_connection_factory = webrtc::CreatePeerConnectionFactory();
+  // something from api
+  rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface>
+    peer_connection_factory = webrtc::CreatePeerConnectionFactory();
 
-// something from api/test
-webrtc::FakeConstraints constraints;
+  // something from api/test
+  webrtc::FakeConstraints constraints;
 
-// something from media/engine
-cricket::WebRtcVideoDeviceCapturerFactory factory;
-std::unique_ptr<cricket::VideoCapturer> capturer = factory.Create(cricket::Device("", 0));
+  // something from media/engine
+  cricket::WebRtcVideoDeviceCapturerFactory factory;
+  std::unique_ptr<cricket::VideoCapturer> capturer = factory.Create(cricket::Device("", 0));
 
-return 0;
+  return 0;
 }
